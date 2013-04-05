97eab3671a8e9d30998486b6b95c1c4dbbc5cb44
Chris Hunt <c@chrishunt.co>
2013-04-04 18:39:19 -0700

    Remove coderwall, I don't use it

--- cw/application.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  // Show coderwall
-  $('#coderwall').proudify({'username':'chrishunt', 'service':'coderwall'});
-
-  // Show GitHub repos
-  $('#github').proudify({'username':'chrishunt', 'service':'github'});
-});
